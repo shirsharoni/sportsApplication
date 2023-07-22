967b26955722bde36c08bdc94d97cb8ec4a41f9b
idobn <ido.bennatan@gmail.com>
2023-07-22 18:19:33 +0300

    balls working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import soccer from './soccer.svg';
+import basketball from './basketball.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          What is your favorite sport?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class='horizontal'>
+          <div class='vertical'>
+            <img src={soccer} className="App-logo" alt="logo" />
+            <Button>
+              Soccer
+            </Button>
+          </div>
+          <div class="vertical">
+            <img src={basketball} className="App-logo" alt="logo" />
+            <Button
+            >
+              Basketball
+            </Button>
+          </div>
+        </div>
       </header>
     </div>
   );
