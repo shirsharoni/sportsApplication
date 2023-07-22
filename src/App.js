99b47109f8a3496033050b777675e28c221b4d60
idobn <ido.bennatan@gmail.com>
2023-07-22 18:45:11 +0300

    router works

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { Routes ,Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import SoccerPage from './components/SoccerPage';
 import BasketballPage from './components/BasketballPage';
 
-function App() {
-  return (
-    <Routes>
-      <Route exact path="/" component={Home} />
-      <Route path="/soccer" component={SoccerPage} />
-      <Route path="/basketball" component={BasketballPage} />
-      {/* Add more routes for other pages as needed */}
-    </Routes>
-  );
-}
 
-export default App;
+import {
+  BrowserRouter as Router,
+  useRoutes,
+} from "react-router-dom";
+
+const App = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/soccer", element: <SoccerPage /> },
+    { path: "/basketball", element: <BasketballPage /> },
+  ]);
+  return routes;
+};
+
+const AppWrapper = () => {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+};
+
+export default AppWrapper;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,45 @@
-// components/Home.js
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import soccer from '../soccer.svg';
+import basketball from '../basketball.svg';
+import '../App.css';
 
-function Home() {
-  return <h1>Welcome to the Home Page!</h1>;
+function App() {
+  function goToSoccer() {
+    console.log('go to soccer');
+    window.location = '/soccer';
+  }
+
+  function goToBasketball() {
+    console.log('go to basketball');
+    window.location = '/basketball';
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          What is your favorite sport?
+        </p>
+        <div class='horizontal'>
+          <div class='vertical'>
+            <img src={soccer} className="App-logo" alt="logo" />
+            <Button onClick={goToSoccer}>
+              Soccer
+            </Button>
+          </div>
+          <div class="vertical">
+            <img src={basketball} className="App-logo" alt="logo" />
+            <Button onClick={goToBasketball}
+            >
+              Basketball
+            </Button>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
-export default Home;
+export default App;
